ffa3997aa9bd738e097566a62cc3fdc8b60205bf
Brandon Magofna <Magofna68@gmail.com>
2022-11-16 14:24:50 -0800

    update searchBar component with basic functionality; :bug: need to hide result table when no results

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -3,24 +3,49 @@ import {MdSearch} from 'react-icons/md';
 import React, {useState} from 'react'
 import CustomButton from '../utility/custom-button/Custom-button';
 import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+
 import {SHOP_DATA} from '../../data';
+import { flexbox } from '@mui/system';
+import { useEffect } from 'react';
 
 export default function SearchBar() {
   const [ searchTerm, setSearchTerm ] = useState('')
+  const [ searchResults, setSearchResults ] = useState([])
 
-  // function searchQuery (searchTerm) {
-  //   const searchedTerm = SHOP_DATA.filter(searchTerm => )
-  // }
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    setSearchTerm(e.target.value);
-    if (searchTerm.length > 0) {
-      return SHOP_DATA.items.name.match(searchTerm)
+  // Loop through Shop Data and pull out all items within categories
+  const categoryItems = []
+  for (let i = 0; i < SHOP_DATA.length; i++) {
+    categoryItems.push(SHOP_DATA[i].items)
+  };
+  // Loop through category items and create array for all itemObj & itemNames
+  const itemNames = []
+  const itemObj = []
+  for (let i = 0; i < categoryItems.length; i++) {
+    for (let j = 0; j < categoryItems[i].length; j++) {
+      itemNames.push(categoryItems[i][j].name);
+      itemObj.push(categoryItems[i][j])
     }
   }
 
-  const results = []
+  function clearResults() {
+      setSearchResults([])
+      setSearchTerm(' ');
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    clearResults()
+
+    const searchedTerm = e.target.value
+    const newTerm = searchedTerm.toString()
+    setSearchTerm(newTerm);
+
+    setSearchResults(itemObj.filter(item =>
+    item.name.toLowerCase().includes(searchTerm)))
+    console.log(searchResults)
+  }
+
   return (
     <Container fluid className='searchBarContain'>
       <div className='searchBar'>
@@ -29,22 +54,63 @@ export default function SearchBar() {
         </button>
         <input 
           placeholder='Search..' 
-          type="text" 
+          type="text"
           name="search" 
-          onChange={handleChange} 
-          value={searchTerm}>
-        </input>
-        <div>
-          {
-            results ?
-            SHOP_DATA.map((item, index) => {
-              <li>{item.name} - {item.price}</li>
-            })
-            :
-            <div>no results</div>
-          }
-        </div>
+          defaultValue={searchTerm}
+          onChange={e => handleChange(e)} 
+        />
       </div>
+      {/* <div> */}
+        <div>
+          { 
+            searchResults ?
+            <Table striped bordered hover variant="dark" sz="md">
+              <thead>
+                <tr> 
+                  {/* <th>Item:</th> */}
+                </tr>
+              </thead>
+              <tbody style={{display: 'block'}}>
+                {/* <div style={{display: 'block'}}> */}
+                {searchResults.map((item, index) => {
+                  return (
+                    <tr>
+                      <td><a href='/'>{item.name}</a></td>
+                    </tr>
+                  )
+                })}
+                {/* </div> */}
+                  </tbody>
+              </Table>
+              :
+              <Table striped bordered hover variant="dark" sz="md">
+              <thead>
+                <tr> 
+                  {/* <th>Item:</th> */}
+                </tr>
+              </thead>
+              <tbody style={{display: 'none'}}>
+                {/* <div style={{display: 'block'}}> */}
+                {searchResults.map((item, index) => {
+                  return (
+                    <tr>
+                      <td><a href='/'>{item.name}</a></td>
+                    </tr>
+                  )
+                })}
+                {/* </div> */}
+                  </tbody>
+              </Table>
+            }
+          {/* {
+            itemNames.map((item, index) => {
+              return(
+                <li>{item}</li>
+              )
+            })
+          } */}
+        </div>
+      {/* </div> */}
     </Container>
   )
 }
